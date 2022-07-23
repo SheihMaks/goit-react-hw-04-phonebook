543b38f84854b48dcdd97c737886e63b2f90e4c1
SheihMaks <92758256+SheihMaks@users.noreply.github.com>
2022-07-24 00:08:05 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,32 +7,16 @@ import { Filter } from "./UserFilter/Filter";
 import { nanoid } from "nanoid";
 
 export const App=()=>{
- const filterId=nanoid();
+  const filterId=nanoid();
  
- const [contacts,setContacts]=useState([]);
- const [filter,setFilter]=useState('');
+ const [contacts,setContacts]=useState(()=>{
+  return JSON.parse(localStorage.getItem('contacts')) ?? []});
  
+  const [filter,setFilter]=useState('');
  
-  // state = {
-  //   contacts: [
-  //     // {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-  //     // {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-  //     // {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-  //     // {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  //   ],
-  //   filter:'',
-  // }
-
-  // componentDidMount(){
-  // const contacts=JSON.parse(localStorage.getItem('contacts'));
-  //   if (contacts){
-  //   this.setState({contacts})}
-  // }
-
-  // componentDidUpdate(prevState){
-  //   if(contacts !== prevState.contacts){
-  //   localStorage.setItem("contacts",JSON.stringify(contacts))}
-  // }
+ useEffect(()=>{
+  localStorage.setItem("contacts",JSON.stringify(contacts))
+},[contacts])
 
 const formHandleSubmit=(data) => {
   data={
